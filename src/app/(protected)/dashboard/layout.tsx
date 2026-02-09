485726035a8ee52921a5fc13d1b5b1e0dd188855
Kamado8421 <lucianomendes8421@gmail.com>
2026-02-09 14:51:47 -0300

    bg-color

--- src/app/(protected)/dashboard/layout.tsx
+++ src/app/(protected)/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import SidebarMember from "@members-components/sidebar";
 export default function LayoutDashboard({ children }: { children: React.ReactNode }) {
 
     return (
-        <div className="flex w-full h-screen bg-gray-100">
+        <div className="flex w-full h-screen bg-[#f9f9f9]">
             <SidebarMember />
             <div className="w-full max-w-425 pt-10 pr-5 pl-10 h-auto">
                 {children}
